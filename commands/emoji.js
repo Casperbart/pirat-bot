23df9d18ae97128baed3b9debf600d10443b80f6
Casperbart <casperbart@gmail.com>
2020-05-04 19:15:48 +0200

    Refactors Emoji alphabet to separate file

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { alphabet, unknown } = require('../assets/alphabet.js');
 
 module.exports = {
     name: 'emoji',
@@ -6,59 +7,17 @@ module.exports = {
     description: "Lav dit navn om til emojis",
     requireArgs: false,
     usage: "",
-    alphabet: {
-        a : "😎",
-        b : "🥓",
-        c : "🤡",
-        d : "🎪",
-        e : "🐘",
-        f : "🌷",
-        g : "🕹️",
-        h : "🕳️",
-        i : "☝",
-        j : "🗾",
-        k : "⌨",
-        l : "💻",
-        m : "🌚",
-        n : "📰",
-        o : "⭕",
-        p : "💩",
-        q : "🎈",
-        r : "🌹",
-        s : "☀️",
-        t : "🌳",
-        u : "👨‍💻",
-        v : "🥒",
-        w : "🧙‍♂️",
-        x : "❌",
-        y : "🔥",
-        z : "🌟",
-        æ : "🤑",
-        ø : "👨‍",
-        å : "🌊" ,
-        "1" : "⌛",
-        "2" : "💿",
-        "3" : "🖊",
-        "4" : "🧫",
-        "5" : "🛡️",
-        "6" : "🗿",
-        "7" : "🧽",
-        "8" : "🚽",
-        "9" : "🚪",
-        "0" : "🪓"
-    },
-    unknownCharacter : "😑",
     execute(msg, args) {
         var emojiName = "Her er dit emojinavn: ";
 
         for(char of msg.author.username.toLowerCase())
         {
-            if(this.alphabet.hasOwnProperty(char)) 
+            if(alphabet.hasOwnProperty(char)) 
             {
-                emojiName += this.alphabet[char];
+                emojiName += alphabet[char];
             } else 
             {
-                emojiName += this.unknownCharacter;
+                emojiName += unknownCharacter;
             }
         }
         
